5b1b3741d96db5a7fca4869343ffa8c803402850
wphu <17106856+williamphu@users.noreply.github.com>
2021-05-27 14:58:45 -0400

    add code to annoatate hardcalls with vep annotations

--- get_data_for_figures.py
+++ get_data_for_figures.py
@@ -59,7 +59,7 @@ def main():
     v2er_hardcalls = v2er_hardcalls.filter_rows(v2er_hardcalls.info.AF[0]<0.001)
     metadata = hl.read_table("gs://gnomad/metadata/exomes/gnomad.exomes.metadata.2018-10-11.ht")
     metadata = metadata.filter(metadata.releasable_2_1) #filter to releaseable samples
-    v2er_data = hl.read_table("gs://gcp-public-data--gnomad/release/2.1.1/ht/exomes/gnomad.exomes.r2.1.1.sites.ht/")
+    v2er = hl.read_table("gs://gcp-public-data--gnomad/release/2.1.1/ht/exomes/gnomad.exomes.r2.1.1.sites.ht/")
 
     if (hl.hadoop_exists("gs://gnomad-tmp/review-hum-mut/random_samples.ht") and hl.hadoop_exists("gs://gnomad-tmp/review-hum-mut/random_samples_hardcalls.mt")):
         random_samples = hl.read_table("gs://gnomad-tmp/review-hum-mut/random_samples.ht")
@@ -69,6 +69,31 @@ def main():
         random_samples, random_samples_hardcalls = get_random_samples_of_populations(populations, 100, metadata, v2er_hardcalls)
         random_samples = random_samples.checkpoint("gs://gnomad-tmp/review-hum-mut/random_samples.ht")
         random_samples_hardcalls = random_samples_hardcalls.checkpoint("gs://gnomad-tmp/review-hum-mut/random_samples_hardcalls.mt")
+
+    #Annotate VEP and other infos
+    random_samples_hardcalls = random_samples_hardcalls.annotate_rows(
+                                vep = v2er[random_samples_hardcalls.locus, random_samples_hardcalls.alleles].vep,
+                                popmax = v2er[random_samples_hardcalls.locus, random_samples_hardcalls.alleles].popmax,
+                                freq = v2er[random_samples_hardcalls.locus, random_samples_hardcalls.alleles].freq
+                            )
+    #Filter to variants of interest
+    
+    #Filter out reference alleles
+    random_samples_hardcalls = random_samples_hardcalls.annotate_rows(samples_with_variant=hl.agg.filter(random_samples_hardcalls.GT.is_non_ref(), hl.agg.collect_as_set(random_samples_hardcalls.s)))
+    ht = random_samples_hardcalls.rows()
+    ht = ht.select(
+    "samples_with_variant",
+    pop=ht.pop,
+    VEP=ht.most_severe_csq, # From https://github.com/broadinstitute/gnomad_methods/blob/35066ffc01d63ac2d7b20e069ea6703013ae9da7/gnomad/utils/vep.py#L484
+    group=ht.group, # You will need to add this annotation based on the groupings that Sanna wants
+    variant=hl.str(ht.locus) + '-' + hl.delimit(ht.alleles, '-'),
+    AC=ht.freq[0].AC,
+    AF=ht.freq[0].AF,
+    AN=ht.freq[0].AN
+    popmax_AC=ht.popmax[0].AC,
+    popmax_AF=ht.popmax[0].AF,
+    popmax_AN=ht.popmax[0].AN,
+)
 
 if __name__ == "__main__":
     main()
