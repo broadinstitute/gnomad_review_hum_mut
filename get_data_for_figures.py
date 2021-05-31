ba7ea47551a54a0692cbcc3e847c930b321de506
wphu <17106856+williamphu@users.noreply.github.com>
2021-05-31 16:43:02 -0400

    add all good updates

--- get_data_for_figures.py
+++ get_data_for_figures.py
@@ -1,10 +1,18 @@
 import argparse
+import logging
 import random
 
 import hail as hl
 from hail.expr.aggregators.aggregators import info_score
 import gnomad
 from gnomad import vep, filtering
+
+logging.basicConfig(
+    format="%(asctime)s (%(name)s %(lineno)s): %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p"
+)
+logger = logging.getLogger("generate_figures")
+logger.setLevel(logging.INFO)
 
 def get_random_subset(n, pop, metadata):
     '''
@@ -88,17 +96,35 @@ def main(args):
 
     #Filter to variants of interest
 
+    if (args.test):
+        random_samples_hardcalls = random_samples_hardcalls._filter_partitions(range(2))
+    
     random_samples_hardcalls = random_samples_hardcalls.filter_rows(
         (~hl.is_defined(random_samples_hardcalls.filters) | (hl.len(random_samples_hardcalls.filters) == 0))
         & (hl.agg.any(random_samples_hardcalls.GT.is_non_ref()))
     )
     random_samples_hardcalls  = filtering.filter_low_conf_regions(random_samples_hardcalls)
-    ##random_samples_hardcalls = random_samples_hardcalls.checkpoint("gs://gnomad-tmp/review-hum-mut/random_samples_hardcalls.mt", overwrite=True)
     random_samples_hardcalls = vep.filter_vep_to_canonical_transcripts(random_samples_hardcalls)
-    ##random_samples_hardcalls = vep.get_most_severe_consequence_for_summary(random_samples_hardcalls)
+    most_severe_csq_summary = vep.get_most_severe_consequence_for_summary(random_samples_hardcalls.rows())[random_samples_hardcalls.row_key]
+    random_samples_hardcalls = random_samples_hardcalls.annotate_rows(
+        most_severe_csq = most_severe_csq_summary.most_severe_csq,
+        protein_coding = most_severe_csq_summary.protein_coding,
+        lof = most_severe_csq_summary.lof,
+        no_lof_flags = most_severe_csq_summary.no_lof_flags
+    ) #get_most_severe_consequence_for_summary works only on tables
     random_samples_hardcalls = filtering.filter_to_adj(random_samples_hardcalls)
     random_samples_hardcalls = random_samples_hardcalls.filter_rows(hl.agg.any(random_samples_hardcalls.GT.is_non_ref())) #possibly repeated above?
-    random_samples_hardcalls = random_samples_hardcalls.checkpoint("gs://gnomad-tmp/review-hum-mut/random_samples_hardcalls-filtered.mt", overwrite=True)
+    random_samples_hardcalls_LOF = random_samples_hardcalls.filter_rows(
+        (random_samples_hardcalls.lof == "HC") &
+        (hl.set(["splice_acceptor", "splice_donor_variant", "stop_gained", "frameshift_variant"]).contains(random_samples_hardcalls.most_severe_csq))
+    )
+    random_samples_hardcalls_LOF = random_samples_hardcalls_LOF.annotate_rows(group = "LoF")
+    random_samples_hardcalls_missense = random_samples_hardcalls.filter_rows(hl.set(["missense_variant", "inframe_deletion", "inframe_insertion"]).contains(random_samples_hardcalls.most_severe_csq))
+    random_samples_hardcalls_missense = random_samples_hardcalls_missense.annotate_rows(group = "missense_indels")
+    random_samples_hardcalls_synonymous = random_samples_hardcalls.filter_rows(hl.set(["synonymous_variant"]).contains(random_samples_hardcalls.most_severe_csq))
+    random_samples_hardcalls_synonymous = random_samples_hardcalls_synonymous.annotate_rows(group = "synonymous")
+    random_samples_hardcalls = random_samples_hardcalls_LOF.union_rows(random_samples_hardcalls_missense, random_samples_hardcalls_synonymous)
+    random_samples_hardcalls = random_samples_hardcalls.checkpoint("gs://gnomad-tmp/review-hum-mut/random_samples_hardcalls_filtered.mt", overwrite=True)
 
     #random_samples_hardcalls = random_samples_hardcalls.filter_rows((hl.len(random_samples_hardcalls.filters) == 0) & hl.agg.any(random_samples_hardcalls.GT.is_non_ref()))
     #random_samples_hardcalls  = vep.filter_low_conf_regions(random_samples_hardcalls)
@@ -114,7 +140,6 @@ def main(args):
     ht = random_samples_hardcalls.rows()
     ht = ht.select(
         "samples_with_variant",
-        pop=ht.pop,
         VEP=ht.most_severe_csq, # From https://github.com/broadinstitute/gnomad_methods/blob/35066ffc01d63ac2d7b20e069ea6703013ae9da7/gnomad/utils/vep.py#L484
         group=ht.group, # You will need to add this annotation based on the groupings that Sanna wants
         variant=hl.str(ht.locus) + '-' + hl.delimit(ht.alleles, '-'),
@@ -125,6 +150,41 @@ def main(args):
         popmax_AF=ht.popmax[0].AF,
         popmax_AN=ht.popmax[0].AN,
     )
+    ht = ht.explode("samples_with_variant")
+    random_samples_pop_map = hl.dict(hl.zip(random_samples.s.collect(),random_samples.pop.collect()))
+    ht = ht.annotate(pop = random_samples_pop_map[ht.samples_with_variant]) #does not require a shuffle this way
+    ht = ht.select(
+        ht.samples_with_variant,
+        ht.pop,
+        ht.VEP,
+        ht.group,
+        ht.variant,
+        ht.AC,
+        ht.AF,
+        ht.AN,
+        ht.popmax_AC,
+        ht.popmax_AF,
+        ht.popmax_AN
+    )
+    '''
+    ht_summary= ht.group_by(ht.samples_with_variant).aggregate(
+        num_Lof = hl.agg.count_where(ht.group=="LoF"),
+        num_missense_indel = hl.agg.count_where(ht.group=="missense_indels"),
+        num_synonymous = hl.agg.count_where(ht.group=="synonymous"),
+        num_total = hl.agg.count()
+    )
+    '''
+    #ht.order_by(ht.pop,ht.samples_with_variant,ht.group)
+    #ht.group_by()
+
+    random_samples.write("gs://gnomad-wphu/review-hum-mut/random_samples_metadata.ht", overwrite=True)
+    random_samples_hardcalls.write("gs://gnomad-wphu/review-hum-mut/random_samples_hardcalls_filtered.mt", overwrite=True)
+    ht.write("gs://gnomad-wphu/review-hum-mut/samples_with_variants.ht", overwrite=True)
+    ht.export("gs://gnomad-wphu/review-hum-mut/samples_with_variants.tsv.bgz", header=True, overwrite=True)
+    #ht_summary.write("gs://gnomad-wphu/review-hum-mut/samples_with_variants_summary.ht", overwrite= True)
+    #ht_summary.export("gs://gnomad-wphu/review-hum-mut/samples_with_variants_summary.tsv.bgz", header=True, overwrite=True)
+
+    logger.info("Wrote out table with %s rows.", ht.count())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -134,6 +194,11 @@ if __name__ == "__main__":
         "--checkpoint",
         action="store_true",
         help="Use previously checkpointed random sample if it exists.")
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help='subset hardcalls table to a small number of partitions for testing.'
+    )
     args = parser.parse_args()
 
     main(args)
