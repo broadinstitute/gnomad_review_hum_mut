295b8d3915f8e7b578b62ae4bae9db2eb2e9e525
wphu <17106856+williamphu@users.noreply.github.com>
2021-09-01 15:33:30 -0400

    address some comments

--- filter_v2_exomes_to_unique.py
+++ filter_v2_exomes_to_unique.py
@@ -12,31 +12,31 @@ logger.setLevel(logging.INFO)
 
 def main(args):
     logger.info("Loading variant table, v2 genomes, v3 variants, and v2 liftover...")
-    samples_with_variants = hl.read_table(args.sample_with_variants_path)
-    original_count = samples_with_variants.count()
+    ht = hl.read_table(args.sample_with_variants_path)
+    original_count = ht.count()
     v2_genomes_ht = hl.read_table("gs://gcp-public-data--gnomad/release/2.1.1/ht/genomes/gnomad.genomes.r2.1.1.sites.ht")
     v3_genomes_ht = hl.read_table("gs://gcp-public-data--gnomad/release/3.1.1/ht/genomes/gnomad.genomes.v3.1.1.sites.ht")
-    v2_exome_liftover_ht` = hl.read_table("gs://gcp-public-data--gnomad/release/2.1.1/liftover_grch38/ht/exomes/gnomad.exomes.r2.1.1.sites.liftover_grch38.ht")
-    v2_liftover = v2_liftover.key_by("original_locus", "original_alleles")
-    v2_liftover_index = v2_liftover[samples_with_variants.locus, samples_with_variants.alleles]
-    samples_with_variants = samples_with_variants.annotate(liftover_locus = v2_liftover_index.locus, liftover_alleles = v2_liftover_index.alleles)
+    v2_exome_liftover_ht = hl.read_table("gs://gcp-public-data--gnomad/release/2.1.1/liftover_grch38/ht/exomes/gnomad.exomes.r2.1.1.sites.liftover_grch38.ht")
+    v2_exome_liftover_ht = v2_exome_liftover_ht.key_by("original_locus", "original_alleles")
+    v2_liftover_index = v2_exome_liftover_ht[ht.key]
+    ht = ht.annotate(liftover_locus = v2_liftover_index.locus, liftover_alleles = v2_liftover_index.alleles)
 
     logger.info("Filtering variants not found in v2 genomes or v3 samples not found in v2 exomes...")
-    idx = v2_genomes.freq_meta.collect()[0].index({'group': 'adj'})
-    samples_with_variants = samples_with_variants.annotate(v2_genomes_adj_freq = v2_genomes[samples_with_variants.locus,samples_with_variants.alleles].freq[idx])
-    v2_genomes_filtering_expr = (samples_with_variants.v2_genomes_adj_freq.AC<1) | ~hl.is_defined(samples_with_variants.v2_genomes_adj_freq.AC)
-    samples_with_variants = samples_with_variants.filter(v2_genomes_filtering_expr)
+    idx = v2_genomes_ht.freq_meta.collect()[0].index({'group': 'adj'})
+    ht = ht.annotate(v2_genomes_adj_freq = v2_genomes_ht[ht.locus,ht.alleles].freq[idx])
+    v2_genomes_filtering_expr = (ht.v2_genomes_adj_freq.AC<1) | ~hl.is_defined(ht.v2_genomes_adj_freq.AC)
+    ht = ht.filter(v2_genomes_filtering_expr)
     freq_adj_idx = v3_genomes_ht.freq_meta.collect()[0].index({'group': 'adj', 'subset': 'non_v2'})
-    samples_with_variants = samples_with_variants.annotate(v3_non_v2_adj_freq = v3_variants[samples_with_variants.liftover_locus,samples_with_variants.liftover_alleles].freq[idx])
-    v3_non_v2_filtering_expr = (samples_with_variants.v3_non_v2_adj_freq.AC<1) | ~hl.is_defined(samples_with_variants.v3_non_v2_adj_freq.AC)
-    samples_with_variants = samples_with_variants.filter(v3_non_v2_filtering_expr)
+    ht = ht.annotate(v3_non_v2_adj_freq = v3_genomes_ht[ht.liftover_locus,ht.liftover_alleles].freq[idx])
+    v3_non_v2_filtering_expr = (ht.v3_non_v2_adj_freq.AC<1) | ~hl.is_defined(ht.v3_non_v2_adj_freq.AC)
+    ht = ht.filter(v3_non_v2_filtering_expr)
 
     logger.info(f"Writing Hail Table to {args.output_path_prefix}/v2_exomes_unique_samples_with_variants.ht")
-    samples_with_variants = samples_with_variants.checkpoint(f"{args.output_path_prefix}/v2_exomes_unique_samples_with_variants.ht", overwrite = args.overwrite)
-    logger.info(f"{original_count} variants were filtered to {samples_with_variants.count()} variants.")
+    ht = ht.checkpoint(f"{args.output_path_prefix}/v2_exomes_unique_samples_with_variants.ht", overwrite = args.overwrite)
+    logger.info(f"{original_count} variants were filtered to {ht.count()} variants.")
     if args.overwrite:
         logger.info(f"Writing TSV to {args.output_path_prefix}/v2_exomes_unique_samples_with_variants.tsv")
-        samples_with_variants.export(
+        ht.export(
             f"{args.output_path_prefix}/v2_exomes_unique_samples_with_variants.tsv",
             header=True,
         )
